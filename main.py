77aaf4301505800ca14fea211f9244551fa2c96a
Nikita Remezov <nikita_remezov_04@mail.ru>
2023-05-16 18:05:25 +0300

    now it can do some interesting but useless things

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import ephem
+from datetime import date
 
 import settings
 
@@ -17,11 +19,33 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+
+def planet_pos(update, context):
+    planet = update.message.text.split()[1]
+    cur_date = date.today()
+
+    planets = {
+        "jupiter": ephem.Jupiter,
+        "moon": ephem.Moon,
+        "saturn": ephem.Saturn,
+        'uranus': ephem.Uranus,
+        'mars': ephem.Mars,
+        'mercury': ephem.Mercury,
+        'venus': ephem.Venus,
+        'sun': ephem.Sun,
+        'pluto': ephem.Pluto,
+        'neptune': ephem.Neptune
+    }
+    const = ephem.constellation(planets[planet.lower()](cur_date))
+    print(const[1])
+    update.message.reply_text(const[1])
+
 def main():
     mybot = Updater(settings.API_KEY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_pos))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
